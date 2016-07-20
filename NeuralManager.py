4ae335a3518de739e74594ca0d02f24a52e9f65b
Andrew Ma <maastergotmail@gmail.com>
2016-07-20 00:35:41 -0400

    added first view

--- NeuralManager.py
+++ NeuralManager.py
@@ -7,13 +7,10 @@ from mako.lookup import TemplateLookup
 
 from twisted.python import log
 
-import os, sys, time
-
-network = ''
-
+import os, sys
 
 root = os.path.join(os.path.dirname(__file__), ".")
-lookup = TemplateLookup(directories=[os.path.join(root, 'views')], 
+lookup = TemplateLookup(directories=[os.path.join(root, 'views')],
 		input_encoding='utf-8',
 		output_encoding='utf-8',
 		default_filters=['decode.utf8'],
@@ -21,31 +18,15 @@ lookup = TemplateLookup(directories=[os.path.join(root, 'views')],
 
 class MainHandler(tornado.web.RequestHandler):
 	def get(self):
-		self.write("Hello, world")
-		#self.write(renderTemplate("myfile.html"))
-
-class StartHandler(tornado.web.RequestHandler):
-    def get(self):
-    	print "Initializing Neural Network"
-    	starttime = time.time()
-    	from mnistManaged import MnistNetwork
-    	network = MnistNetwork()
-    	print "wowe"
-    	self.write("time taken: " + str(time.time() - starttime))
-        # self.write("Hello, world")
-
-
-class TrainHandler(tornado.web.RequestHandler):
-    def get(self):
-    	print "We got something"
-        # self.write("Hello, world")
+		# self.write("Hello, world")
+		self.render("views/index.html")
 
 def renderTemplate(templateName, **kwargs):
 	template = lookup.get_template(templateName)
 	args = []
-	try: 
+	try:
 		return template.render(*args, **kwargs)
-	except Exception, e:	
+	except Exception, e:
 		print e
 
 if __name__ == "__main__":
@@ -54,8 +35,6 @@ if __name__ == "__main__":
 
 	app = tornado.web.Application([
 		(r"/", MainHandler),
-		(r"/start", StartHandler),
-		(r"/train", TrainHandler),
 		(r"/static/(.*)", tornado.web.StaticFileHandler, {'path': os.path.join(root, 'static')})
 	])
 
