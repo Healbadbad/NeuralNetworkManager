b9592708d4d4c41496b469b98be351884dedf32c
Nathan Blanka <healbadbad@yahoo.com>
2016-07-20 19:35:17 -0500

    added snapshot route, reorganized

--- NeuralManager.py
+++ NeuralManager.py
@@ -1,7 +1,7 @@
 import tornado.ioloop
 import tornado.web
 from tornado import gen
-from tornado.queues import LifoQueue
+from tornado.queues import Queue
 
 from mako import exceptions
 from mako.template import Template
@@ -13,7 +13,7 @@ import os, sys, time
 
 network = ''
 initialized = False
-actionQueue = LifoQueue()
+actionQueue = Queue()
 
 root = os.path.join(os.path.dirname(__file__), ".")
 lookup = TemplateLookup(directories=[os.path.join(root, 'views')], 
@@ -22,20 +22,43 @@ lookup = TemplateLookup(directories=[os.path.join(root, 'views')],
 		default_filters=['decode.utf8'],
 		module_directory=os.path.join(root, 'tmp/mako'))
 
+
+#############################
+#
+#	Network Functions
+#
+#############################
+
 def initNetwork():
 	from mnistManaged import MnistNetwork
 	app.network = MnistNetwork()
-	print app.network
-
 	app.initialized = True
-	# log. "WEEEWOOOO"
 
 def train():
 	if app.initialized == False:
 		print "network not yet initialized"
 		return
 	app.network.train()
-	print "Done yay"
+
+def validation():
+	if app.initialized == False:
+		print "network not yet initialized"
+		return
+	app.val_acc = app.network.val_acc()
+
+def snapshot():
+	if app.initialized == False:
+		print "network not yet initialized"
+		return
+	print sys.getsizeof(app.network)
+	app.snapshot = app.network.snapshot()
+
+
+#############################
+#
+#	Request Endpoints
+#
+#############################
 
 class MainHandler(tornado.web.RequestHandler):
 	def get(self):
@@ -49,12 +72,7 @@ class StartHandler(tornado.web.RequestHandler):
 		print "Initializing Neural Network"
 		starttime = time.time()
 		yield actionQueue.put(initNetwork)
-		print "wowe"
-
-		# print self.application
 		self.write("time taken: " + str(time.time() - starttime))
-
-		# self.write("Hello, world")
 
 
 class TrainHandler(tornado.web.RequestHandler):
@@ -65,7 +83,12 @@ class TrainHandler(tornado.web.RequestHandler):
 		yield actionQueue.put(train)
 		self.write("time taken: " + str(time.time() - starttime))
 
-		# self.write("Hello, world")
+class SnapshotHandler(tornado.web.RequestHandler):
+	def get(self):
+		print "Getting a snapshot"
+		print sys.getsizeof(app.network)
+		print app.network
+		self.write("Network Snapshot: " + str(app.snapshot))
 
 class StopHandler(tornado.web.RequestHandler):
 	@gen.coroutine
@@ -74,8 +97,6 @@ class StopHandler(tornado.web.RequestHandler):
 		starttime = time.time()
 		yield actionQueue.put(train)
 		self.write("time taken: " + str(time.time() - starttime))
-
-		# self.write("Hello, world")
 
 def renderTemplate(templateName, **kwargs):
 	template = lookup.get_template(templateName)
@@ -105,10 +126,12 @@ if __name__ == "__main__":
 		(r"/", MainHandler),
 		(r"/start", StartHandler),
 		(r"/train", TrainHandler),
+		(r"/snapshot", SnapshotHandler),
 		(r"/static/(.*)", tornado.web.StaticFileHandler, {'path': os.path.join(root, 'static')})
 	], autoreload=True)
 
 	app.initialized = False
+	app.snapshot = 'No Snapshot'
 	app.listen(8888)
 	tornado.ioloop.IOLoop.current().spawn_callback(consumer)
 	tornado.ioloop.IOLoop.current().start()
