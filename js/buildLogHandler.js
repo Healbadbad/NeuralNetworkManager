a316fb06e1ad96066e76d71e1f0a9da02a901cec
Nathan Blank <healbadbad@yahoo.com>
2016-07-22 19:59:54 -0500

    oops

---
+++ js/buildLogHandler.js
@@ -0,0 +1,14 @@
+var ws = new WebSocket("ws://localhost:80/buildSocket");
+
+ws.onopen = function() {
+	console.log("Build Log connected!");
+};
+
+ws.onmessage = function (evt) {
+	// console.log("received message: " + evt.data);
+	var target = document.getElementById("logFeed");
+	var div = document.createElement('div');
+	target.innerHTML = target.innerHTML + evt.data; // clear existing
+	var elements = div.childNodes;
+	// span.appendChild(text);
+};
