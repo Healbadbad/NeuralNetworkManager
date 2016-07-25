69f7c44a1754ba364a092e3979390ce439464644
Andrew Ma <maastergotmail@gmail.com>
2016-07-25 00:32:49 -0400

    changed buttons to search dropdown

--- js/modelListHandler.js
+++ js/modelListHandler.js
@@ -7,11 +7,17 @@ ws.onopen = function() {
 
 ws.onmessage = function (evt) {
 	var fileList = evt.data.substring(1, evt.data.length-1).split(', ');
-	var target = document.getElementById("model-list");
+	var target = document.getElementById("model-list-wrapper");
 	for(var key in fileList) {
-		var button = document.createElement('button');
-		button.className = 'ui inverted button';
-		button.innerHTML = fileList[key].substring(1, fileList[key].length-1);
-		target.appendChild(button);
+		var item = document.createElement('div');
+		item.className = 'item';
+		item.innerHTML = fileList[key].substring(1, fileList[key].length-1);
+		target.appendChild(item);
 	}
 };
+
+$(document).ready(function() {
+    $('.ui.dropdown').dropdown({
+	    maxSelections: 3
+	});
+});
